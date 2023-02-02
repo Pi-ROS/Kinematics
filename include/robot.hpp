9c281220206c2629c3a3319dc6bd2ac24c67f09c
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2023-02-02 09:21:09 +0100

    sketch of the Robot class

---
+++ include/robot.hpp
@@ -0,0 +1,73 @@
+#ifndef JACOBIAN_HPP
+#define JACOBIAN_HPP
+
+#include <Eigen/Dense>
+#include "kinematics.hpp"
+
+typedef Eigen::Matrix<double, 6, 6> Jacobian;
+typedef Eigen::Matrix<double, 6, 1> JointStateVector;
+typedef Eigen::Matrix<double, 4, 4> SE3;
+
+class Robot {
+
+private:
+    // Transformation matrix for a pure translation about the x-axis
+    SE3 Tx(double d);
+    // Transformation matrix for a pure translation about the y-axis
+    SE3 Ty(double d);
+    // Transformation matrix for a pure translation about the z-axis
+    SE3 Tz(double d);
+    // Transformation matrix for a pure rotation about the x-axis
+    SE3 Rx(double th);
+    // Transformation matrix for a pure rotation about the y-axis
+    SE3 Ry(double th);
+    // Transformation matrix for a pure rotation about the z-axis
+    SE3 Rz(double th);
+
+    // Derivative of a pure translation about the x-axis, with respect to 
+    // the joint coordinate
+    SE3 dTx(double d);
+    // Derivative of a pure translation about the y-axis, with respect to
+    // the joint coordinate
+    SE3 dTy(double d);
+    // Derivative of a pure translation about the z-axis, with respect to
+    // the joint coordinate
+    SE3 dTz(double d);
+    // Derivative of a pure rotation about the x-axis, with respect to
+    // the joint coordinate
+    SE3 dRx(double th);
+    // Derivative of a pure rotation about the y-axis, with respect to
+    // the joint coordinate
+    SE3 dRy(double th);
+    // Derivative of a pure rotation about the z-axis, with respect to
+    // the joint coordinate
+    SE3 dRz(double th);
+    // Inverse skew operation. Takes a 3x3 skew-symmetric matrix and returns
+    // a 3x1 vector.
+    Eigen::Matrix<double, 3, 1> invskew(Eigen::Matrix<double, 3, 3> &S);
+
+public:
+    // Joint state
+    JointStateVector q;
+    // Constructor
+    Robot(JointStateVector q);
+    // Transformation matrix from the base frame to frame 1
+    SE3 T01();
+    // Tranformation matrix from frame 1 to frame 2
+    SE3 T12();
+    // Transformation matrix from frame 2 to frame 3
+    SE3 T23();
+    // Transformation matrix from frame 3 to frame 4
+    SE3 T34();
+    // Transformation matrix from frame 4 to frame 5
+    SE3 T45();
+    // Transformation matrix from frame 5 to frame 6
+    SE3 T56();
+    // Transformation matrix from the base frame to the end-effector
+    SE3 forwardKinematics();
+    // Jacobian of the end-effector transformation with respect to
+    // joint coordinates
+    Jacobian jacobian();
+}
+
+#endif
---
+++ src/robot.cpp
@@ -0,0 +1,128 @@
+#include "../include/robot.hpp"
+#include <cmath>
+
+/**
+ * @brief: Computation of the Jacobian matrix for the transformation from
+ * the base frame to the end effector frame, with respect to the joints coordinates.
+*/
+
+Jacobian Robot::Jacobian() {
+    // Angular velocities
+    Eigen::Matrix<double, 3, 1> Jw1, Jw2, Jw3, Jw4, Jw5, Jw6;
+    // Linear velocities
+    Eigen::Matrix<double, 3, 1> Jv1, Jv2, Jv3, Jv4, Jv5, Jv6;
+    Jacobian J;
+
+    // TODO: compute the jacobian for the UR5
+    return J;
+}
+
+/**
+ * @brief: Constructor for the Robot class.
+ * @param: q - the joint state vector
+ */
+
+Robot::Robot(JointsStateVector q) {
+    this->q = q;
+}
+
+/**
+ * @brief: Derivative of the transformation matrix for a pure translation
+ * about the x-axis.
+ * @param: d - the joint coordinate
+ */
+SE3 Robot::dTx(double d) {
+    SE3 dTx;
+    dTx << 0, 0, 0, 1,
+           0, 0, 0, 0,
+           0, 0, 0, 0,
+           0, 0, 0, 0;
+    return dTx;
+}
+
+/**
+ * @brief: Derivative of the transformation matrix for a pure translation
+ * about the y-axis.
+ * @param: d - the joint coordinate
+ */
+
+SE3 Robot::dTy(double d) {
+    SE3 dTy;
+    dTy << 0, 0, 0, 0,
+           0, 0, 0, 1,
+           0, 0, 0, 0,
+           0, 0, 0, 0;
+    return dTy;
+}
+
+/**
+ * @brief: Derivative of the transformation matrix for a pure translation
+ * about the z-axis.
+ * @param: d - the joint coordinate
+ */
+
+SE3 Robot::dTz(double d) {
+    SE3 dTz;
+    dTz << 0, 0, 0, 0,
+           0, 0, 0, 0,
+           0, 0, 0, 1,
+           0, 0, 0, 0;
+    return dTz;
+}
+
+/**
+ * @brief: Derivative of the transformation matrix for a pure rotation
+ * about the x-axis.
+ * @param: th - the joint coordinate
+ */
+
+SE3 Robot::dRx(double th) {
+    SE3 dRx;
+    dRx << 0, 0, 0, 0,
+           0, 0, -1, 0,
+           0, 1, 0, 0,
+           0, 0, 0, 0;
+    return dRx * Rx(th);
+}
+
+/**
+ * @brief: Derivative of the transformation matrix for a pure rotation
+ * about the y-axis.
+ * @param: th - the joint coordinate
+ */
+
+SE3 Robot::dRy(double th) {
+    SE3 dRy;
+    dRy << 0, 0, 1, 0,
+           0, 0, 0, 0,
+           -1, 0, 0, 0,
+           0, 0, 0, 0;
+    return dRy * Ry(th);
+}
+
+/**
+ * @brief: Derivative of the transformation matrix for a pure rotation
+ * about the z-axis.
+ * @param: th - the joint coordinate
+ */
+
+SE3 Robot::dRz(double th) {
+    SE3 dRz;
+    dRz << 0, -1, 0, 0,
+           1, 0, 0, 0,
+           0, 0, 0, 0,
+           0, 0, 0, 0;
+    return dRz * Rz(th);
+}
+
+/**
+ * @brief: Inverse skew operation. Takes a 3x3 skew-symmetric matrix and
+ * returns a 3x1 vector.
+ * @param: S - the skew-symmetric matrix
+ */
+
+Eigen::Matrix<double, 3, 1> Robot::invskew(Eigen::Matrix<double, 3, 3> &S) {
+    Eigen::Matrix<double, 3, 1> v;
+    v << S(2, 1), S(0, 2), S(1, 0);
+    return v;
+}
