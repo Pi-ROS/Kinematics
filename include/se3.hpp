cce54fc6fbf88da5cfec8d27c800f54e9cd434b7
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2023-02-03 16:35:54 +0100

    removed code for computing unnecessary derivatives

--- include/se3.hpp
+++ include/se3.hpp
@@ -19,31 +19,6 @@ class SE3Operations{
         static SE3 Tz(double d);
         static SO3 rot(SE3 T);
         static VEC3 tra(SE3 T);
-        
 };
-
-
-// // Derivative of a pure translation about the x-axis, with respect to
-// // the joint coordinate
-// SE3 dTx(double d);
-// // Derivative of a pure translation about the y-axis, with respect to
-// // the joint coordinate
-// SE3 dTy(double d);
-// // Derivative of a pure translation about the z-axis, with respect to
-// // the joint coordinate
-// SE3 dTz(double d);
-// // Derivative of a pure rotation about the x-axis, with respect to
-// // the joint coordinate
-// SE3 dRx(double th);
-// // Derivative of a pure rotation about the y-axis, with respect to
-// // the joint coordinate
-// SE3 dRy(double th);
-// // Derivative of a pure rotation about the z-axis, with respect to
-// // the joint coordinate
-// SE3 dRz(double th);
-// // Inverse skew operation. Takes a 3x3 skew-symmetric matrix and returns
-// // a 3x1 vector.
-// Eigen::Matrix<double, 3, 1> invskew(Eigen::Matrix<double, 3, 3> &S);
-// Extract the rotation matrix from a homogeneous transformation matrix
 
 #endif
